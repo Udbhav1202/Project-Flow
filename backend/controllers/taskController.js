8fc331076d2d6d192cad189a4078f00b6fcda477
Udbhav <101322451+Usri12@users.noreply.github.com>
2026-02-07 22:44:43 +0530

    Update task

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -28,4 +28,28 @@ const getTasksByProject = async (req, res) => {
   }
 };
 
-module.exports = { createTask, getTasksByProject };
+const updateTaskStatus = async (req, res) => {
+  try {
+    const { status } = req.body;
+
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    if (task.assignedTo.toString() !== req.user.id) {
+      return res.status(403).json({ message: "Not allowed" });
+    }
+
+    task.status = status;
+    await task.save();
+
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+module.exports = { createTask, getTasksByProject, updateTaskStatus };
