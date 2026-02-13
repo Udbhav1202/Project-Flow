3eaebc9c7b367e65cf33b4b998e53856129fd85a
Udbhav <101322451+Usri12@users.noreply.github.com>
2026-02-13 18:56:26 +0530

    Adding Service layer and async Handler

--- backend/services/taskService.js
+++ backend/services/taskService.js
@@ -1,5 +1,5 @@
 const Task = require("../models/taskModel");
-const Project = require("../models/Project");
+const Project = require("../models/projectModel");
 const AppError = require("../utils/AppError");
 
 const createTaskService = async ({ title, projectId, assignedTo, userId }) => {
@@ -22,6 +22,49 @@ const createTaskService = async ({ title, projectId, assignedTo, userId }) => {
   return task;
 };
 
+const getTasksByProjectService = async ({ projectId, page }) => {
+  const limit = 5;
+  const skip = (page - 1) * limit;
+
+  const total = await Task.countDocuments({ projectId });
+
+  const tasks = await Task.find({ projectId })
+    .skip(skip)
+    .limit(limit)
+    .populate("assignedTo", "name email")
+    .populate("projectId", "title");
+
+  return {
+    totalTasks: total,
+    currentPage: page,
+    totalPages: Math.ceil(total / limit),
+    tasks
+  };
+};
+
+const updateTaskStatusService = async ({ taskId, status, userId }) => {
+  const allowedStatus = ["todo", "in-progress", "done"];
+
+  if (!allowedStatus.includes(status)) {
+    throw new AppError("Invalid status", 400);
+  }
+
+  const task = await Task.findById(taskId);
+
+  if (!task) throw new AppError("Task not found", 404);
+
+  if (task.assignedTo.toString() !== userId) {
+    throw new AppError("Only assigned user can update status", 403);
+  }
+
+  task.status = status;
+  await task.save();
+
+  return task;
+};
+
 module.exports = {
   createTaskService,
+  getTasksByProjectService,
+  updateTaskStatusService
 };
