55f73976b72e3f56db2dff3c946ea2b3077610b8
Udbhav <101322451+Usri12@users.noreply.github.com>
2026-02-13 18:56:08 +0530

    Adding Service layer and async Handler

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,82 +1,45 @@
 const Task = require('../models/taskModel');
 const Project = require("../models/projectModel");
 const AppError = require("../utils/AppError");
-
-const createTask = async (req, res, next) => {
-  try {
-    const task = await createTaskService({
-      title: req.body.title,
-      projectId: req.body.projectId,
-      assignedTo: req.body.assignedTo,
-      userId: req.user.id,
-    });
-
-    res.status(201).json(task);
-  } catch (error) {
-    next(error);
-  }
-};
-
-const getTasksByProject = async (req, res, next) => {
-  try {
-
-    const page = parseInt(req.query.page) || 1;
-    const limit = 5;
-    const skip = (page - 1) * limit;
-
-    const total = await Task.countDocuments({
-      projectId: req.params.projectId
-    });
+const asyncHandler = require("../utils/asyncHandler");
+const {
+  createTaskService,
+  getTasksByProjectService,
+  updateTaskStatusService
+} = require("../services/taskService");
 
 
-    //
-    const tasks = await Task.find({
-      projectId: req.params.projectId
-    })
-    .skip(skip)
-    .limit(limit)
-    .populate("assignedTo", "name email")
-    .populate("projectId", "title");
+const createTask = asyncHandler(async (req, res) => {
+  const task = await createTaskService({
+    title: req.body.title,
+    projectId: req.body.projectId,
+    assignedTo: req.body.assignedTo,
+    userId: req.user.id,
+  });
 
-    res.json({
-      totalTasks: total,
-      currentPage: page,
-      totalPages: Math.ceil(total / limit),
-      tasks
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  res.status(201).json(task);
+});
 
-const updateTaskStatus = async (req, res, next) => {
-  try {
-    const { status } = req.body;
+const getTasksByProject = asyncHandler(async (req, res) => {
+  const data = await getTasksByProjectService({
+    projectId: req.params.projectId,
+    page: Math.max(parseInt(req.query.page) || 1, 1)
+  });
 
-    const allowedStatus = ["todo", "in-progress", "done"];
+  res.json(data);
+});
 
-    if (!allowedStatus.includes(status)) {
-      throw new AppError("Invalid status value", 400);
-    }
 
-    const task = await Task.findById(req.params.id);
+const updateTaskStatus = asyncHandler(async (req, res) => {
+  const task = await updateTaskStatusService({
+    taskId: req.params.id,
+    status: req.body.status,
+    userId: req.user.id
+  });
 
-    if (!task) {
-      throw new AppError("Task not found", 404);
-    }
+  res.json(task);
+});
 
-    if (task.assignedTo.toString() !== req.user.id) {
-      throw new AppError("Only assigned user can update task status", 403);
-    }
-
-    task.status = status;
-    await task.save();
-
-    res.json(task);
-  } catch (error) {
-    next(error);
-  }
-};
 
 
 
