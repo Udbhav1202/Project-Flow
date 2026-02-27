243616238d58fda80c753ae4dd47d9f8715fd65d
Udbhav <101322451+Usri12@users.noreply.github.com>
2026-02-27 17:31:23 +0530

    Modification for service layer

--- backend/App.js
+++ backend/App.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const dotenv =  require('dotenv').config();
+const cors = require("cors");
 const connectDB = require('./config/db');
 const projectRoutes = require('./routes/projectRoutes');
 const userRoutes  = require('./routes/userRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 const errorHandler = require("./middleware/errorHandler");
 
-const app = express()
+const app = express();
+app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
 connectDB();
