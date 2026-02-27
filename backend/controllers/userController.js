00bfe2a5c6a70210a8983aec860181fdf52c9363
Udbhav <101322451+Usri12@users.noreply.github.com>
2026-02-27 17:32:45 +0530

    Modification for service layer

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,73 +1,26 @@
-const User = require('../models/userModel');
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const AppError = require("../utils/AppError");
+const asyncHandler = require('../utils/asyncHandler');
+const {
+  registerUserService,
+  loginUserService
+} = require('../services/userService');
 
-const registerUser = async (req, res, next) => {
-  try {
-    const { name, email, password } = req.body;
-
-    if (!name || !email || !password) {
-      return next(new AppError("Please enter all fields", 400));
-    }
-
-    const userExist = await User.findOne({ email });
-    if(userExist){
-        return next(new AppError("User already exist", 400));
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({
-        name,
-        email,
-        password: hashedPassword,
-    });
-
-    if (user) {
-      res.status(201).json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        token: generateToken(user._id),
-      });
-    } else {
-      return next(new AppError("Invalid user data", 400));
-    }
-  } catch (error) {
-    next(error);
-  }
-};
-
-const loginUser = async (req, res, next) => {
-  
-  try{
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-
-    if (user && (await bcrypt.compare(password, user.password))) {
-      res.json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        token: generateToken(user._id),
-        message: "Login Successful",
-      });
-    } else {
-      return next(new AppError("Invalid email or password", 401));
-    }
-
-  } catch(error){
-    next(error);
-  }
-  
-};
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "30d", 
+const registerUser = asyncHandler(async (req, res) => {
+  const userData = await registerUserService({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
   });
-};
 
-module.exports = {registerUser, loginUser};
+  res.status(201).json(userData);
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const userData = await loginUserService({
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  res.json(userData);
+});
+
+module.exports = { registerUser, loginUser };
